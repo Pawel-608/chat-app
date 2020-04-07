d36b65cf1988da1b82ec5beb1e5ac2f7a92e8f4d
Pawel-608 <pawel.rainer@gmail.com>
2020-04-07 19:32:05 +0200

    emit msg

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,13 @@ let socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to server');
+
+    // socket.emit('createMsg', { from: "WDJ", text: "What's going on?" })
 });
 socket.on('disconnect', () => {
      console.log('disconnet to server');
 });
+
+socket.on('newMessage', msg => {
+    console.log(msg);
+})
--- server/server.js
+++ server/server.js
@@ -10,15 +10,40 @@ let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+
 io.on('connection', socket => {
     console.log('New user just connected');
 
     socket.on('createMessage', msg => {
-        console.log(msg);
-    })
+        // console.log(msg);
+
+        socket.emit('newMessage', {
+            from: "admin",
+            text: "Welcome to the chat app!",
+            createdAt: new Date().getTime()
+        });
+        socket.broadcast.emit('newMessage', {
+            from: "admin",
+            text: "New user join",
+            createdAt: new Date().getTime()
+        });
+
+        // socket.broadcast.emit('newMsg', { 
+        //         from: msg.from,
+        //         text: msg.text,
+        //         createdAt: new Date().getTime()
+        //     });// EVERYBODT (WITHOUT EMMITER) WILL GET THIS MSG
+        // io.emit('newMsg', { 
+        //     from: msg.from,
+        //     text: msg.text,
+        //     createdAt: new Date().getTime()
+        //  });// SEND TO ALL USERS
+    });
+
+    // socket.emit('newMsg', { from: "WDJ", text: "What's going on?" });// SEND TO 1 USER
 
     socket.on('disconnect', socket => {
-        console.log('New user just disonnected');
+        console.log('User just disonnected');
     });
 });
 
