997934ad2ed4dde9e2b12ca8284d3005fbb75fa9
Pawel-608 <pawel.rainer@gmail.com>
2020-04-07 18:45:44 +0200

    test

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '/../', 'public');
-const app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', socket => {
+    console.log('New user just connected');
+
+    socket.on('createMessage', msg => {
+        console.log(msg);
+    })
+
+    socket.on('disconnect', socket => {
+        console.log('New user just disonnected');
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`listen on port: ${PORT}`);
 });
